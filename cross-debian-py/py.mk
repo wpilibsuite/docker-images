93566f617a54d77f6d1dbbc08aa06ac2c787fb4f
Gold856 <117957790+Gold856@users.noreply.github.com>
2026-05-30 01:51:07 -0400

    Update to 2027 opensdk toolchains

--- cross-debian-py/py.mk
+++ cross-debian-py/py.mk
@@ -5,8 +5,8 @@ DOCKER_USER?=wpilib
 
 TYPE_SYSTEMCORE=systemcore
 VERSION_SYSTEMCORE=2027
-TARGET_HOST_SYSTEMCORE=aarch64-bookworm-linux-gnu
-AC_TARGET_HOST_SYSTEMCORE=aarch64-bookworm-linux-gnu
+TARGET_HOST_SYSTEMCORE=aarch64-trixie-linux-gnu
+AC_TARGET_HOST_SYSTEMCORE=aarch64-trixie-linux-gnu
 
 
 .PHONY: build/cross-python
